bac69b873f971bd7df51acd3495bd9038570e11a
galue99 <eespinetti_99@hotmail.com>
2015-03-31 23:04:09 -0430

    comentario

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    io = require('socket.io'),
+    http = require('http'),
+    app = express(),
+    server = http.createServer(app),
+    io = io.listen(server);
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.send('Hello World!');
+});
+
+app.get('/incidente', function(req, res){
+	res.send('Hello World');
 });
 
 app.listen(app.get('port'), function() {
